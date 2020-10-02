402e440c4d1365e126e539f15dc80d5a4d07f522
upendraiscookan@gmial.com <upendraiscookan@gmail.com>
2020-10-02 22:41:27 +0530

    Updates

---
+++ precache-manifest.f11719a8a06e0c530ef463015bbb5adf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88c414a7f2b97c3576be8f4f195d2d5d",
+    "url": "/editor/index.html"
+  },
+  {
+    "revision": "3d290bd39fda32e08794",
+    "url": "/editor/static/css/main.fc93ca60.chunk.css"
+  },
+  {
+    "revision": "21909767cd496f91db65",
+    "url": "/editor/static/js/2.c6ecd675.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/editor/static/js/2.c6ecd675.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d290bd39fda32e08794",
+    "url": "/editor/static/js/main.79f5bb42.chunk.js"
+  },
+  {
+    "revision": "b2497aff8511b7867fa1",
+    "url": "/editor/static/js/runtime-main.eca0e832.js"
+  }
+]);
